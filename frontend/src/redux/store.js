93231bddb88734b8c38a304ec45faa7468f6ae04
ejfschmittel <ejfschmittel@aol.de>
2021-08-27 18:46:04 +0200

    added base styles and redux base

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+
+import reducers from './root-reducer'
+
+const INITIAL_STATE = {}
+const enhancers = []
+const middleware = [
+    thunk,
+]
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
+
+const store = createStore(
+    reducers,
+    INITIAL_STATE,
+    composedEnhancers
+)
+
+export default store
