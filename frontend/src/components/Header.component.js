d93cdc9b94992eef0acc73ff203a18eaed388b86
ejfschmittel <ejfschmittel@aol.de>
2021-10-18 18:00:36 +0200

    added responsive nav bar

--- frontend/src/components/Header.component.js
+++ frontend/src/components/Header.component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import userActions from "../redux/user/user.actions"
 import "../styles/components/Header.scss";
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom"
 
 const Header = () => {
     const dispatch = useDispatch()
+    const [navOpen, setNavOpen] = useState(false)
 
     const user = useSelector(store => store.user.user);
 
@@ -13,21 +14,33 @@ const Header = () => {
         dispatch(userActions.logout("/"))
     }
 
+    const toggleNav = () => setNavOpen(!navOpen)
+
     return (
         <header className="header">
             <div className="header__content">
                 <div className="header__title">
                     <Link to="/">GuessThePerson</Link>
                 </div>
-                <nav className="header__navigation">
+
+                <nav className={`header__navigation ${navOpen ? "header__navigation--open" : ""}`}>
 
 
+                <button className="header__menu-btn" onClick={toggleNav}>
+                    <div className="header__btn-bars">
+                        <div className="header__btn-bar header__btn-bar--1"></div>
+                        <div className="header__btn-bar header__btn-bar--2"></div>
+                        <div className="header__btn-bar header__btn-bar--3"></div>
+                    </div>
+                </button>
+
+                <div className="header__links">
                     {user ? 
                         <React.Fragment>
                             <Link className="header__link" to="/">Home</Link>
                             <Link className="header__link" to="/people">People</Link>
                             <Link className="header__link" to="/boards">Boards</Link>
-                            <button className="header__link" onClick={onLogoutClick}>Logout</button>
+                            <a className="header__link" onClick={onLogoutClick}>Logout</a>
                         </React.Fragment>
                         :
                         <React.Fragment>
@@ -35,6 +48,7 @@ const Header = () => {
                             <Link className="header__link" to="/register">Register</Link>
                         </React.Fragment>
                     }
+                </div>
 
                     
                 </nav>      
